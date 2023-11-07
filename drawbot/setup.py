faece8674facbf89a628ed8f3bdc56cd51761f9a
Andrew Dassonville <dassonville.andrew@gmail.com>
2023-11-07 11:19:58 -0800

    Add drawbot package

---
+++ drawbot/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'drawbot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf', glob('urdf/*.xacro')),
+        ('share/' + package_name + '/urdf', glob('urdf/*.urdf')),
+        ('share/' + package_name + '/meshes/visual', glob('meshes/visual/*.stl')),
+        ('share/' + package_name + '/meshes/collision', glob('meshes/collision/*.stl')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='andrewda',
+    maintainer_email='dassonville.andrew@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+    },
+)
